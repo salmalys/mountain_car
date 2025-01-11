4e0098cd3cdfad4efa4a653574db7a3f0020e328
salmalys <salmalahrach27@gmail.com>
2025-01-11 15:52:02 +0100

    Reorg

--- src/train.py
+++ src/train.py
@@ -6,7 +6,7 @@ env = gymnasium.make("MountainCar-v0")
 agent = SemiGradientSarsa(env, encode_fct=mc_tile_encoding)
 
 # Entraînement
-agent.train(nb_episodes=1000, alpha=0.1, gamma=0.99, epsilon=0.001)
+agent.train(nb_episodes=1000, alpha=0.1, gamma=0.99, epsilon=0.001, verbose = True)
 
 # # Sauvegarder les paramètres
 # agent.save_params("params/weights.npy")
